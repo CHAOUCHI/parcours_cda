fad4cce1fe9666afe11e73ade26eaa6bb73d8c4a
CHAOUCHI <massi.chaouchi@gmail.com>
2025-02-26 09:04:40 +0100

    ajout cours ci/cd sans ssh

--- NodeJS/Chapitre 1 - Express/exemple/app.js
+++ NodeJS/Chapitre 1 - Express/exemple/app.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const { del } = require("request");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin : [
-        "todolist.com"
-    ]
-}));
+app.use(cors());
 
 const products = [
     {name : "Nike",price : 30},
     {name : "Converse",price : 100},
     {name : "Adidias", price : 30},
 ];
+
 app.get("/products",(req,res)=>{
     res.json(products);
 })
+
+app.get(parseIdToInt,"/product/:id",(req,res)=>{
+
+    res.json(products[req.params.id]);
+});
 
 app.post("/product",(req,res)=>{
     const newProduct = req.body;
@@ -24,4 +27,33 @@ app.post("/product",(req,res)=>{
     res.json(newProduct);
 });
 
+app.put(parseIdToInt,"/product/:id",(req,res)=>{
+    const newProductValues = req.body;
+    const product = products[req.params.id] = {
+        ...products[req.params.id],
+        ...req.body
+    };
+    res.json({id:req.params.id,product});
+});
+
+
+app.delete(parseIdToInt,"/product/:id",(req,res)=>{
+
+    const deletedProducts = products.splice(id,1);
+    
+    res.json(deletedProducts);
+});
+
 app.listen(3030);
+
+
+function parseIdToInt(req,res,next){
+    const id = parseInt(req.params.id);
+    if(id===NaN) {
+        res.status(400).json({id : req.params.id});
+        return;
+    }
+    req.params.id = id;
+
+    next();
+}
